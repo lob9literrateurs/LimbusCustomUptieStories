9b5056e959d48e807589c18cf5136eb99d6ccebb
lob9literrateurs <cannithpowercrystals@gmail.com>
2026-08-08 05:06:03 -0400

    Add files via upload
    waow

---
+++ LetheMain.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using Lethe.Patches;
+
+namespace CustomMirror;
+
+[BepInPlugin(GUID, NAME, VERSION)]
+public class Main : BasePlugin
+{
+    public const string GUID = $"{AUTHOR}.{NAME}";
+    public const string NAME = "CustomMirror";
+    public const string VERSION = "0.0.1";
+    public const string AUTHOR = "lob";
+
+    public override void Load()
+    {
+        Harmony harmony = new Harmony(NAME);
+
+        // Load all Harmony patches in this assembly.
+        harmony.PatchAll();
+
+        Log.LogInfo("[Lethe] Harmony patches loaded.");
+    }
+}
